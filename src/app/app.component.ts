ca33b5b970b1c5acd894f99ae8b291b8a22ab0f3
tomaszbabinski <tommybab@gmail.com>
2019-11-21 23:37:43 +0100

    Angular exercises - binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TouchSequence } from 'selenium-webdriver';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TaskList';
+  newTask: string;
+  tasksList: Array<string> = [];
+  tasksDone: Array<string> = [];
+
+  add() {
+    this.tasksList.push(this.newTask);
+    this.newTask = '';
+    console.log(this.tasksList);
+  }
+
+  remove(task: string) {
+    this.tasksList = this.tasksList.filter(e => e !== task);
+
+  }
+
+  done(task: string) {
+    this.tasksDone.push(task);
+    this.remove(task);
+    console.log(this);
+  }
 }
